16887fa70a53764fcd504f3cab8cf1ddd50b2afc
James Trimble <jtrim.ons@gmail.com>
2023-11-03 11:52:08 +0000

    Include population types in all-classifications.json

--- python-scripts/combine-all-dims.py
+++ python-scripts/combine-all-dims.py
@@ -1,15 +1,38 @@
 """Create a JSON file with details for all classifications."""
 
+import copy
 import glob
 import json
 
-all_classifications = {}
+poptypes = ["UR", "UR_HH"]
 
-for filename in glob.glob("downloaded/classifications-*.json"):
-    with open(filename, 'r') as f:
-        data = json.load(f)
-    for item in data["items"]:
-        all_classifications[item["id"]] = item
+all_classifications = {}
+all_classifications_by_poptype = {poptype: {} for poptype in poptypes}
+
+for poptype in poptypes:
+    for filename in glob.glob("downloaded/classifications-{}/*.json".format(poptype)):
+        with open(filename, 'r') as f:
+            data = json.load(f)
+        for item in data["items"]:
+            all_classifications_by_poptype[poptype][item["id"]] = copy.deepcopy(item)
+            all_classifications[item["id"]] = item
+
+for classification in all_classifications:
+    all_classifications[classification]['poptypes'] = [
+        p for p in poptypes if classification in all_classifications_by_poptype[p]
+    ]
+
+# Check that classifications with the same name for UR and UR_HH agree
+for classification in all_classifications_by_poptype["UR"]:
+    if classification in all_classifications_by_poptype["UR_HH"]:
+        if (
+            json.dumps(all_classifications_by_poptype["UR"][classification]) !=
+            json.dumps(all_classifications_by_poptype["UR_HH"][classification])
+        ):
+            raise Exception('UR and UR_HH disagree on ' + classification)
+
+with open('generated/all-classifications-by-poptype.json', 'w') as f:
+    json.dump(all_classifications_by_poptype, f)
 
 with open('generated/all-classifications.json', 'w') as f:
     json.dump(all_classifications, f)
