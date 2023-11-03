c5b95196beadf2296bf37b08c4ad14a207270cc3
James Trimble <jtrim.ons@gmail.com>
2023-11-03 11:50:30 +0000

    Fix shortcuts to work for both UR and UR_HH

--- show-categories.sh
+++ show-categories.sh
@@ -9,7 +9,7 @@ for f in input-txt-files/input-classifications.txt input-txt-files/output-classi
     echo
     cat $f | while read c; do
         echo $c
-        filename=$(rg -l $c downloaded/classifications-*.json | head -n1)
+        filename=$(rg -l $c downloaded/classifications-*/*.json | head -n1)
         echo $filename
         cat $filename | jq ".items[] | select(.id == \"$c\") | .categories[] | .label"
         echo $(cat $filename | jq ".items[] | select(.id == \"$c\") | .categories[] | .label" | wc -l) categories.
