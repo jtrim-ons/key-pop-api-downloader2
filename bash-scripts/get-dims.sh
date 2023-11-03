d8ab9478e49b5de68eab44097564ddff1db95255
James Trimble <jtrim.ons@gmail.com>
2023-11-03 11:48:04 +0000

    Get UR_HH dimensions

--- bash-scripts/get-dims.sh
+++ bash-scripts/get-dims.sh
@@ -8,10 +8,14 @@ mkdir -p downloaded
 
 curl -o downloaded/poptypes.json 'https://api.beta.ons.gov.uk/v1/population-types?limit=100'
 
-curl -o downloaded/dimensions.json 'https://api.beta.ons.gov.uk/v1/population-types/UR/dimensions?limit=100'
+for poptype in UR UR_HH; do
+    curl -o downloaded/dimensions-$poptype.json "https://api.beta.ons.gov.uk/v1/population-types/$poptype/dimensions?limit=200"
 
-jq '.items[] | .id' downloaded/dimensions.json | tr -d '"' | while read dim; do
-    echo $dim
-    curl -o downloaded/classifications-$dim.json "https://api.beta.ons.gov.uk/v1/population-types/UR/dimensions/$dim/categorisations"
-    sleep 0.8
+    mkdir -p downloaded/classifications-$poptype
+
+    jq '.items[] | .id' downloaded/dimensions-$poptype.json | tr -d '"' | while read dim; do
+        echo $dim
+        curl -o downloaded/classifications-$poptype/$dim.json "https://api.beta.ons.gov.uk/v1/population-types/$poptype/dimensions/$dim/categorisations"
+        sleep 0.8
+    done
 done
